873559af11c9b16d017ae128f4bb80e616d12aee
Rafael O. Freire <120592511+rfreir3@users.noreply.github.com>
2023-02-18 11:34:44 -0300

    Update Preenchendo matriz dinamicamente.c

--- Preenchendo matriz dinamicamente.c
+++ Preenchendo matriz dinamicamente.c
@@ -6,19 +6,29 @@ respectivo ´ındice (matriz[i][j] = i*3 + j). Em seguida, imprima a matriz.*/
 
 int main(){
     int linha=3, coluna=3;
-    int**M=(int**)malloc(linha*sizeof(int*));
+    int**M=(int**)malloc(linha*sizeof(int*));//Alocando vetor que guarda os ponteiros de outros vetores (que corresponde as linhas da matriz);
+    
     for(int linhas=0;linhas<linha;linhas++){
         M[linha]=(int*)malloc(coluna*sizeof(int));
     }
+    //============================================
+    //alocando vetores que são guardados dentro dos vetores que correspondem as linhas da matriz(os quais correspondem as colunas da matriz);
     for(int i=0;i<linha;i++){
         for(int j=0;j<coluna;j++){
             M[i][j]=i*3+j;
             printf("M[%d][%d]=%d\n", i, j, M[i][j]);
         }
     }
+    //==============================================
+    //armazenando valores dentro da matriz;
+    
     for(int i=0;i<linha;i++){
             free(M[i]);
     }
+    //=======================
+    //liberando espaços da memória das colunas dentro das linhas;
     free(M);
+    //======
+    //liberando linhas;
     return(0);
 }
